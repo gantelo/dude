d763084cc85eaabea24110fede07585dd454e350
gantelo <gabriel1536@gmail.com>
2020-10-28 23:16:34 -0300

    🎉Taking care of business

---
+++ README.md
@@ -0,0 +1,10 @@
+# Just a dude in full css
+
+### `yarn start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+Will display my "face" rolling around
+
+![alt text](./src/who/face.jpg "Foteli, para vos mamá")
---
+++ src/components/dude.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles.css";
+
+const Dude = () => {
+  return (
+    <div className="spinner">
+      <div className="container">
+        <div className="hair hair1" />
+        <div className="hair hair2" />
+        <div className="hair hair3" />
+        <div className="hair hair4" />
+        <div className="face" />
+        <div className="leftEyeBrow" />
+        <div className="leftEyeBrowHide" />
+        <div className="rightEyeBrow" />
+        <div className="rightEyeBrowHide" />
+        <div className="leftEye" />
+        <div className="rightEye" />
+        <div className="ear" />
+        <div className="leftCheeck" />
+        <div className="rightCheeck" />
+        <div className="nose" />
+        <div className="mouth1" />
+        <div className="mouth2" />
+        <div className="hair beard1" />
+        <div className="hair beard2" />
+        <div className="hair beard3" />
+        <div className="hair beard4" />
+      </div>
+    </div>
+  );
+};
+
+export default Dude;
